6ee03088c21877ff035acb700eb0649947bfd42d
diakite Ibrahima sory <diakitemouslim2021@gmail.com>
2021-02-06 04:58:27 +0000

    mon commit suivant

--- lib/src/api_files/home_api.dart
+++ lib/src/api_files/home_api.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:progress_indicator_button/button_stagger_animation.dart';
+import 'package:progress_indicator_button/progress_button.dart';
+
 
 
 class SwiperPage extends StatefulWidget {
@@ -56,6 +59,99 @@ class _SwiperPageState extends State<SwiperPage> {
           ),
         ],
       ),
+    );
+  }
+}
+
+
+class ButtonStaggerAnimation extends StatelessWidget {
+  // Animation fields
+  final AnimationController controller;
+
+  // Display fields
+  final Color color;
+  final Color progressIndicatorColor;
+  final double progressIndicatorSize;
+  final BorderRadius borderRadius;
+  final double strokeWidth;
+  final Function(AnimationController) onPressed;
+  final Widget child;
+
+  ButtonStaggerAnimation({
+    Key key,
+    this.controller,
+    this.color,
+    this.progressIndicatorColor,
+    this.progressIndicatorSize,
+    this.borderRadius,
+    this.onPressed,
+    this.strokeWidth,
+    this.child,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: _progressAnimatedBuilder);
+  }
+
+  Widget _buttonChild() {
+    if (controller.isAnimating) {
+      return Container();
+    } else if (controller.isCompleted) {
+      return OverflowBox(
+        maxWidth: progressIndicatorSize,
+        maxHeight: progressIndicatorSize,
+        child: CircularProgressIndicator(
+          strokeWidth: strokeWidth,
+          valueColor: AlwaysStoppedAnimation<Color>(progressIndicatorColor),
+        ),
+      );
+    }
+    return child;
+  }
+
+  AnimatedBuilder _progressAnimatedBuilder(
+      BuildContext context, BoxConstraints constraints) {
+    var buttonHeight = (constraints.maxHeight != double.infinity)
+        ? constraints.maxHeight
+        : 60.0;
+
+    var widthAnimation = Tween<double>(
+      begin: constraints.maxWidth,
+      end: buttonHeight,
+    ).animate(
+      CurvedAnimation(
+        parent: controller,
+        curve: Curves.easeInOut,
+      ),
+    );
+
+    var borderRadiusAnimation = Tween<BorderRadius>(
+      begin: borderRadius,
+      end: BorderRadius.all(Radius.circular(buttonHeight / 2.0)),
+    ).animate(CurvedAnimation(
+      parent: controller,
+      curve: Curves.easeInOut,
+    ));
+
+    return AnimatedBuilder(
+      animation: controller,
+      builder: (context, child) {
+        return SizedBox(
+          height: buttonHeight,
+          width: widthAnimation.value,
+          child: RaisedButton(
+            shape: RoundedRectangleBorder(
+              borderRadius: borderRadiusAnimation.value,
+            ),
+            color: color,
+            child: _buttonChild(),
+            onPressed: () {
+              this.onPressed(controller);
+            },
+          ),
+        );
+      },
     );
   }
 }
